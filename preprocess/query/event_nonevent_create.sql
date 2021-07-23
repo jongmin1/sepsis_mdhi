85338f4f8a0632e3fb8a8590a9bbef881b0477a5
qkrwlgml05 <48074138+qkrwlgml05@users.noreply.github.com>
2021-07-23 16:39:46 +0900

    Update event_nonevent_create.sql

--- preprocess/query/event_nonevent_create.sql
+++ preprocess/query/event_nonevent_create.sql
@@ -51,30 +51,7 @@ on p.subject_id = s.subject_id;
 -- 	--> 발병 환자와 정상 환자 나누어 테이블 저장 
 
 -- 발병 환자 
---	1. 발병 환자 subject_id, stay_id 추출 
-create table public.infection as
-with s as (
-	select 
-		subject_id,
-		hadm_id,
-		min(suspected_infection_time) as infection_time
-	from sepsis.suspicion_of_infection 
-	where suspected_infection = 1
-	group by subject_id, hadm_id
-)
-select
-	distinct s.subject_id,
-	r.stay_id,
-	s.infection_time
-from 
-	s inner join public.realage r
-	on (s.subject_id = r.subject_id and s.hadm_id = r.hadm_id)
-	inner join hosp.diagnoses_icd d
-	on s.subject_id = d.subject_id
-where 
-	d.icd_code in  ('99592', 'R652', 'R6520', 'R6521');
-
---	2. 정상 환자테이블 이용하여 sofa_table에서 데이터 추출 (발병 시간에 따라 추출)  
+--	정상 환자테이블 이용하여 sofa_table에서 데이터 추출 (발병 시간에 따라 추출)  
 create table public.event_table as
 select i.subject_id, ns.*, i.infection_time, 
 (case when i.infection_time between ns.starttime and ns.endtime then 1 else null end) as is_infection,
