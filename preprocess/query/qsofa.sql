8f89e2b1a2a8e422eb6f685638b668c7b345506b
qkrwlgml05 <48074138+qkrwlgml05@users.noreply.github.com>
2021-08-04 09:14:58 +0900

    Update qsofa.sql

--- preprocess/query/qsofa.sql
+++ preprocess/query/qsofa.sql
@@ -39,7 +39,7 @@ group by qt.stay_id, hr, starttime, endtime;
 create table public.qsofa as
 select * from qsofa
 
--- 3. icd code 
+-- 3. abp_mean, heart_rate, SaO2의 값을 chartevents에서 뽑아내는 테이블
 create temp table chartevent as
 select subject_id, hadm_id, stay_id, charttime, 
 avg(case when itemid = 220052 then valuenum end) as abp_mean,
@@ -49,15 +49,19 @@ from icu.chartevents c
 where itemid in (220052, 220045, 220227, 220277)
 group by subject_id, hadm_id, stay_id, charttime 
 
+-- 4. icd 진단코드를 붙이기 위해서
+-- 	4-1. chartevents에서 subject_id, hadm_id, stay_id의 쌍 한 쌍씩만 가진다
 create temp table ids as
 select distinct subject_id, hadm_id, stay_id 
 from icu.chartevents c 
 
+-- 	4-2. qsofa 테이블에 subject_id, hadm_id, stay_id 합하기
 create temp table qsofa_ids as
 select i.subject_id, i.hadm_id, q.* from public.qsofa q
 left join ids i
 on q.stay_id = i.stay_id
 
+-- 5. qsofa_ids와 chartevent 테이블 (4-2와 3번 테이블)을 합한 뒤 fio2를 합하는 테이블
 create temp table qsofa_all as
 with qsofa_sum as
 (
@@ -72,6 +76,7 @@ from qsofa_sum qs
 left join sepsis.bg b 
 on b.subject_id = qs.subject_id and b.hadm_id = qs.hadm_id and b.charttime > qs.starttime and b.charttime <= qs.endtime
 
+-- 6. subject_id, hadm_id, stay_id로 묶어 avg값으로 나타내는 테이블
 create table public.qsofa_features as
 select subject_id, hadm_id, stay_id, hr, starttime, endtime,
 avg(sbp) as sbp, min(gcs_min) as gcs_min, avg(respiratory_rate) as respiratory_rate, 
@@ -79,6 +84,7 @@ avg(abp_mean) as abp_mean, avg(heart_rate) as heart_rate, avg(SaO2) as SaO2, avg
 from qsofa_all
 group by subject_id, hadm_id, stay_id, starttime, endtime, hr
 
+-- 4-3. 전체 테이블에서(6번 테이블) icd 코드를 합하여 그 값이 있는 경우(진단을 받은 경우) 1, 아닌 경우 0을 return 하여 테이블 완성
 create table public.qsofa_icdcode as
 select distinct qf.*, (case when di.icd_code is null then 0 else 1 end) as icd_event from public.qsofa_features qf
 left join hosp.diagnoses_icd di 
